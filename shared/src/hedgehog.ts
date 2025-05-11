a70c8152b18a1e8d9deac6bc6148539c13b58e26
Toni Haapanen <toni.haapanen@druid.fi>
2025-05-11 09:47:11 +0300

    Hedgehog schema

--- shared/src/hedgehog.ts
+++ shared/src/hedgehog.ts
@@ -5,7 +5,14 @@ import { z } from "zod";
  */
 
 export const hedgehogSchema = z.object({
-  id: z.number(), // TODO: loput siilin tietomallista. Zod:lta löytyy esimerkiksi tällaisia tyyppejä: z.enum(), z.string(), z.number() jne. joita voi olla tarpeen hyödyntää
+  id: z.number().optional(), // ID is optional for new hedgehogs
+  name: z.string().min(1),
+  age: z.number().int().nonnegative("Age must be zero or positive"),
+  gender: z.enum(["male", "female", "unknown"]),
+  location: z.object({
+    type: z.literal("Point"),
+    coordinates: z.array(z.number()).length(2)
+  })
 });
 
 export type Hedgehog = z.infer<typeof hedgehogSchema>;
